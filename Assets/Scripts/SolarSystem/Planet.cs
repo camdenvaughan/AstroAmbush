ee058578b3595644d7df9cdd102a47ea10f387c3
camden9 <camdenvaughan@knights.ucf.edu>
2021-08-25 16:51:45 -0400

    Planet Generation
    Followed Sebastian Lague tutorial for planet generation. Now can create planets in editor. Need to refactor to allow instanciating of random planets

--- Assets/Scripts/SolarSystem/Planet.cs
+++ Assets/Scripts/SolarSystem/Planet.cs
@@ -17,7 +17,8 @@ public class Planet : MonoBehaviour
     [HideInInspector] public bool shapeSettingsFoldOut;
     [HideInInspector] public bool colorSettingsFoldOut;
 
-    private ShapeGenerator shapeGenerator;
+    private ShapeGenerator shapeGenerator = new ShapeGenerator();
+    private ColorGenerator colorGenerator = new ColorGenerator();
     
     [SerializeField, HideInInspector]
     private MeshFilter[] meshFilters;
@@ -30,7 +31,8 @@ public class Planet : MonoBehaviour
 
     void Initialize()
     {
-        shapeGenerator = new ShapeGenerator(shapeSettings);
+        shapeGenerator.UpdateSettings(shapeSettings);
+        colorGenerator.UpdateSettings(colorSettings);
         if (meshFilters == null || meshFilters.Length == 0)
         {
             meshFilters = new MeshFilter[6];
@@ -45,12 +47,13 @@ public class Planet : MonoBehaviour
             {
                 GameObject meshObj = new GameObject("mesh");
                 meshObj.transform.parent = transform;
-                
-                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+
+                meshObj.AddComponent<MeshRenderer>();
                 meshFilters[i] = meshObj.AddComponent<MeshFilter>();
                 meshFilters[i].sharedMesh = new Mesh();
             }
 
+            meshFilters[i].GetComponent<MeshRenderer>().sharedMaterial = colorSettings.planetMaterial;
             terrainFaces[i] = new TerrainFace(shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
         }
     }
@@ -87,14 +90,12 @@ public class Planet : MonoBehaviour
         {
             face.ConstructMesh();
         }
+        colorGenerator.UpdateElevation(shapeGenerator.elevationMinMax);
     }
 
     void GenerateColors()
     {
-        foreach (MeshFilter meshFilter in meshFilters)
-        {
-            meshFilter.GetComponent<MeshRenderer>().sharedMaterial.color = colorSettings.planetColor;
-        }
+        colorGenerator.UpdateColors();
     }
     
 }
